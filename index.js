00dd720bbcd54c5926df609f6de01ea2ac3a3477
Wanlop Korwachiraphan <sosweets@gmail.com>
2021-09-08 00:00:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ setTimeout(() => {
 },1000)
 
 console.log('2')
+
++++++++++++++++++++++
+    
+function random() {
+    return Math.random()  
+}
+console.log(random())
+
+++++++++++++++++++++++++
+call back
+
+// function random() {
+//     setTimeout(()=>{
+//          return Math.random()
+//     },1000)
+     
+// }
+// console.log(random())
+
+function random(cb) {
+    setTimeout(()=>{
+        cb(Math.random())
+    },3000)
+}
+
+random((result)=>{
+    console.log(result)
+})
+
++++++++++++++++++++++
+
+
